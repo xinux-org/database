5cb94ffcb2f9d62f5b8ee4f22c5ee462298a1399
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-12-06 22:10:54 +0500

    fix: package referenced on dev

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         devShells.default = mkShell {
           packages = [
             brotli
-            generator.defaultPackage.${system}
+            generator.packages.${system}.default
           ];
         };
       });
