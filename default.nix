f0d6eed6baae643182f3adfb7d461e2cd5432ea0
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-01-13 20:48:16 +0500

    refactor: remove commented bash script

--- default.nix
+++ default.nix
@@ -29,21 +29,6 @@ in
           fi
         fi
       done
-
-      # for Line in $(cat versions)
-      # do
-      #   PREV_VERSION=$(cat "$Line/nixpkgs.ver")
-      #   RUST_LOG=generator=trace nix develop --command -- generator -v "$Line" --src "$Line" 1>&2
-      #   NEW_VERSION=$(cat "$Line/nixpkgs.ver")
-      #   if [ "$PREV_VERSION" != "$NEW_VERSION" ]; then
-      #     if [ -f "$Line/nixpkgs.db" ]; then
-      #       brotli "./$Line/nixpkgs.db" -o "./$Line/nixpkgs.db.br" -v -f 1>&2
-      #     fi
-      #     if [ -f "$Line/nixpkgs_versions.db" ]; then
-      #       brotli "./$Line/nixpkgs_versions.db" -o "./$Line/nixpkgs_versions.db.br" -v -f 1>&2
-      #     fi
-      #   fi
-      # done
     '';
 
     meta = {
