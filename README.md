40c8300f8bfd88a5264d597a5f7bbc93fc2e24ca
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-01-13 19:51:59 +0500

    refactor: use flake-parts and do commits correctly

---
+++ README.md
@@ -0,0 +1,37 @@
+<p align="center">
+    <img src=".github/assets/header.png" alt="Xinux'es {Database}">
+</p>
+
+<p align="center">
+    <h3 align="center">Pre-generated nixpkgs sqlite databases.</h3>
+</p>
+
+<p align="center">
+    <img align="center" src="https://img.shields.io/github/languages/top/xinux-org/database?style=flat&logo=nixos&logoColor=5277C3&labelColor=ffffff&color=ffffff" alt="Top Used Language">
+    <a href="https://t.me/xinux"><img align="center" src="https://img.shields.io/badge/chat-grey?style=flat&logo=telegram&logoColor=5277C3&labelColor=ffffff&color=ffffff" alt="Telegram Community"></a>
+</p>
+
+## About
+
+This repository holds pre-generated, ready to use sqlite databases that contains package meta & details derived from `nixpkgs`. This repository serves our GTK software as a source of truth to obrain data about packages. Our runners run every at 12:00 and keeps databases updated.
+
+## Branches
+
+We have the following branches of `nixpkgs`:
+
+- nixos-25.05
+- nixos-25.11
+- nixos-unstable
+- nixpkgs-unstable
+
+## How to add
+
+If you need to add more branches, send PR or update the list in `versions` file.
+
+## License
+
+This project is licensed under the MIT license - see the [LICENSE](LICENSE) file for details.
+
+<p align="center">
+    <img src=".github/assets/footer.png" alt="Xinux'es {Database}">
+</p>
--- flake.nix
+++ flake.nix
@@ -1,33 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    # Stable for keeping thins clean
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    # Fresh and new for testing
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # The flake-parts library
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Generator for database generation
     generator.url = "github:xinux-org/generator";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      utils,
-      generator,
-    }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+  outputs = {
+    self,
+    flake-parts,
+    generator,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        # Overlay for pkgs
+        _module.args.pkgs = import self.inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              xinux-generator = generator.packages.${system}.default;
+            })
+          ];
+          config.allowUnfree = true;
+        };
+
         # Nix script formatter
-        formatter = pkgs.nixfmt-rfc-style;
+        formatter = pkgs.alejandra;
 
         # Development environment
         devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.brotli
-            generator.packages.${system}.default
+          packages = with pkgs; [
+            brotli
+            xinux-generator
           ];
         };
-      }
-    );
+      };
+    });
 }
